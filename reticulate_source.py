61a5c80fc52374aba5885724ef70a63260bc5627
Benjamin Van Court <b.s.vancourt@wustl.edu>
2024-11-10 17:52:39 -0600

    Update reticulate_source.py
    Now reloads modules

--- reticulate_source.py
+++ reticulate_source.py
@@ -1,8 +1,13 @@
 import os
 import sys
+import importlib
 sys.path.insert(0, os.path.join(os.path.split(os.path.abspath(__file__))[0], "plotomata"))
 
 import plotters
 import colors
+
+importlib.reload(plotters)
+importlib.reload(colors)
+
 from plotters import *
 from colors import *
