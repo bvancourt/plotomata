2a5f548cd01738c16be430cd3c28852211c80e6f
Benjamin Van Court <b.s.vancourt@wustl.edu>
2024-11-10 21:11:41 -0600

    added doctring to reticulate_source.py

--- reticulate_source.py
+++ reticulate_source.py
@@ -1,13 +1,26 @@
+"""
+The purpose of this file is to allow effectively importing and repeatedly
+reloading the package in R using
+
+reticulate::source_python("/path/to/plotomata/reticulate_source.py")
+
+This is a development/debugging feature only needed to repeatedly modify 
+plotomata code and test it in R.
+"""
+
 import os
 import sys
 import importlib
-sys.path.insert(0, os.path.join(os.path.split(os.path.abspath(__file__))[0], "plotomata"))
+
+sys.path.insert(
+    0, os.path.join(os.path.split(os.path.abspath(__file__))[0], "plotomata")
+)
 
 import plotters
-import colors
+import color_sets
 
 importlib.reload(plotters)
-importlib.reload(colors)
+importlib.reload(color_sets)
 
 from plotters import *
-from colors import *
+from color_sets import *
