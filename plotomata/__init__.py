0328b694985d9df81ab23c2d1080ed58d9e1c118
Benjamin Van Court <b.s.vancourt@wustl.edu>
2024-11-11 23:23:14 -0600

    Update __init__.py
    Added categorical_scatter import

--- plotomata/__init__.py
+++ plotomata/__init__.py
@@ -1,5 +1,5 @@
 """
-Plotomata is a package for automating generation of publication style/quality 
+Plotomata is a package for automating generation of publication style/quality
 plots. At this stage in development, it's use is not recommended, but it will be
 great eventually, maybe.
 """
@@ -11,7 +11,7 @@ import importlib
 importlib.reload(plotters)
 importlib.reload(color_sets)
 
-from .plotters import bar_plot, column_plot
+from .plotters import bar_plot, column_plot, categorical_scatter
 from .color_sets import tab20_colors, nb50_colors
 
 __version__ = "0.0.0"
