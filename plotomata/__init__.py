1e8af1906174941283712f259ebc84d5b8a96854
Benjamin Van Court <b.s.vancourt@wustl.edu>
2024-11-18 00:26:40 -0600

    Update __init__.py
    Added utils and style_packets modules

--- plotomata/__init__.py
+++ plotomata/__init__.py
@@ -1,17 +1,23 @@
 """
 Plotomata is a package for automating generation of publication style/quality
 plots. At this stage in development, it's use is not recommended, but it will be
-great eventually, maybe.
+great eventually.
 """
 
 from . import plotters
 from . import color_sets
+from . import style_packets
+from . import utils
 import importlib
 
 importlib.reload(plotters)
 importlib.reload(color_sets)
+importlib.reload(style_packets)
+importlib.reload(utils)
 
-from .plotters import bar_plot, column_plot, categorical_scatter
+from .plotters import bar_plot, column_plot, scatter_plot
 from .color_sets import tab20_colors, nb50_colors
+from .style_packets import StylePacket
+from .utils import PassthroughDict
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
