f901df243c57bef23aa67530f37663a18da08655
Benjamin Van Court <b.s.vancourt@wustl.edu>
2024-12-12 22:39:14 -0600

    Update test_color_palettes.py

--- tests/test_color_palettes.py
+++ tests/test_color_palettes.py
@@ -3,31 +3,41 @@ import pytest
 import numpy as np
 from plotomata.color_palettes import Color
 
+
 @pytest.fixture
 def transparent_magenta():
     return Color(255, 0, 255, 0)
+
 
 def test_Color_input_types():
     a = Color([1, 2, 3])
     b = Color((1, 2, 3, 255))
     c = Color("#010203ff")
     d = Color("0x010203")
-    e = Color(1/255, 2/255, 3/255, 1.)
-    f = Color(np.array((1/255, 2/255, 3/255, 1.)))
+    e = Color(1 / 255, 2 / 255, 3 / 255, 1.0)
+    f = Color(np.array((1 / 255, 2 / 255, 3 / 255, 1.0)))
     g = Color(np.array([1, 2, 3, 255]))
 
     assert a == b == c == d == e == f == g
 
+
 def test_Color_hsv_adjust(transparent_magenta):
-    assert transparent_magenta.adjust_hue(.5) == transparent_magenta.adjust_hue(-.5)
-    assert transparent_magenta.adjust_hue(.5) != transparent_magenta.adjust_hue(-.6)
-    assert transparent_magenta.adjust_saturation(-.5) == Color((1., .5, 1., 0.))
-    assert transparent_magenta/2 == transparent_magenta*.5
+    assert transparent_magenta.adjust_hue(
+        0.5
+    ) == transparent_magenta.adjust_hue(-0.5)
+    assert transparent_magenta.adjust_hue(
+        0.5
+    ) != transparent_magenta.adjust_hue(-0.6)
+    assert transparent_magenta.adjust_saturation(-0.5) == Color(
+        (1.0, 0.5, 1.0, 0.0)
+    )
+    assert transparent_magenta / 2 == transparent_magenta * 0.5
+
 
 def test_Color_properties(transparent_magenta):
     with pytest.raises(Exception):
         # Colors are immutable
-        transparent_magenta.alpha = 1.
+        transparent_magenta.alpha = 1.0
 
     # Colors are tuples
     assert isinstance(transparent_magenta, tuple)
